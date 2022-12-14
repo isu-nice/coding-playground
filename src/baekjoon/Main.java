f55eb6e2d5511edc68f005e5563a87c1a283e93d
Isu Shin <isu010614@naver.com>
2022-12-14 21:40:29 +0900

    solve problem

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,29 +1,70 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Main {
-	boolean solution(String s) {
-		boolean answer = true;
-		int len = s.length();
+public class Main {
 
-		Queue<Character> q = new LinkedList<>();
+	public static void main(String[] args) throws IOException {
 
-		for(int i = 0; i < len; i++) {
-			q.add(s.charAt(i));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		// 카운팅 정렬
+		double mean;
+		int median, mode, range;
+		int[] arr = new int[n];
+		int[] countingArr = new int[8001];
+
+		int sum = 0;
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+			countingArr[arr[i] + 4000]++;
 		}
 
-		int cntP = 0;  int cntY = 0;
-		for(int i = 0; i < len; i++) {
-			int alphabet = q.poll();
-			if(alphabet == 'p' || alphabet == 'P') cntP++;
-			if(alphabet == 'y' || alphabet == 'Y') cntY++;
+		int a = 0;
+		int countingMax = 0;
+		int[] sortedArr = new int[n];
+		int[] sortedArrCount = new int[n];
+		for(int i = 0; i < 8001; i++) {
+
+			if(countingArr[i] >= 1) {
+				sortedArrCount[a] = countingArr[i]; // sortedArrCount에 sortedArr의 중복횟수를 저장
+
+				for(int cnt = 1; cnt <= countingArr[i]; cnt++) { // counting된 횟수만큼 sortedArr에 저장
+					sortedArr[a] = i - 4000;
+					a++;
+				}
+
+				if(countingArr[i] > countingMax) { // 중복횟수 max값 저장
+					countingMax = countingArr[i];
+				}
+			}
 		}
 
-		if(cntP != cntY) return false;
+		// 최빈값 저장
+		Queue<Integer> q = new LinkedList<>();
+		for(int i = 0; i < n; i++) {
+			if(sortedArrCount[i] == countingMax) {
+				q.offer(sortedArr[i]);
+			}
+		}
 
-		return answer;
+		if(q.size() > 1) q.poll();
+
+		br.close();
+		mean = (double)sum / n;
+		median = sortedArr[(n-1)/2];
+		mode = q.peek();
+		range = sortedArr[n-1] - sortedArr[0];
+
+		System.out.println(Math.round(mean));
+		System.out.println(median);
+		System.out.println(mode);
+		System.out.println(range);
 	}
-
 }
