59031bc3cefe0137145447091f3fc8e2e0547aaf
Isu Shin <isu010614@naver.com>
2022-12-28 15:56:51 +0900

    feat: solve problem

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,39 +3,23 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        HashSet<Character> set = new HashSet<>();
-        int count = n;
+        int answer = fibonacci(N);
+        System.out.println(answer);
 
-        for(int i = 1; i <= n; i++) {
-            String word = br.readLine();
-            set.add(word.charAt(0));
+    }
 
-            for(int a = 1; a < word.length(); a++) {
+    public static int fibonacci(int N) {
 
-                if(word.charAt(a-1) == word.charAt(a)) {
-                    set.add(word.charAt(a));
-                }else {
+        if (N <= 1) return N;
 
-                    if(!set.contains(word.charAt(a))) {
-                        set.add(word.charAt(a));
-                    }else {
-                        count--;
-                        break;
-                    }
-                }
-            }
-            set.clear();
-        }
-
-        System.out.println(count);
+        return fibonacci(N - 2) + fibonacci(N - 1);
     }
 }
