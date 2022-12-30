20e1e478876b2d6c44ebc016ad285ba839dc7bf1
Isu Shin <isu010614@naver.com>
2022-12-30 23:36:48 +0900

    feat: solve problem

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,25 +3,54 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static final int NUMBER_OF_SIDES = 3;
+    private static final String RIGHT_SIGN = "right" + "\n";
+    private static final String WRONG_SIGN = "wrong" + "\n";
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder signs = new StringBuilder();
 
-        int hour = Integer.parseInt(st.nextToken());
-        int minute = Integer.parseInt(st.nextToken());
-        int time = Integer.parseInt(br.readLine());
+        String lengths = console.readLine();
+        int[] sideLengths = getSideLengths(lengths);
 
-        int rest = (minute + time) % 60;
-        hour += (minute + time - rest) / 60;
-        minute = rest;
+        while (!isExitSignal(sideLengths)) {
+            if (isRightTriangle(sideLengths)) {
+                signs.append(RIGHT_SIGN);
+            } else {
+                signs.append(WRONG_SIGN);
+            }
+            lengths = console.readLine();
+            sideLengths = getSideLengths(lengths);
+        }
 
-        if (hour >= 24) hour = hour - 24;
+        System.out.print(signs);
+        console.close();
+    }
 
-        System.out.println(hour + " " + minute);
+    private static boolean isExitSignal(int[] sideLengths) {
+        return Arrays.stream(sideLengths).sum() == 0;
+    }
+
+    private static boolean isRightTriangle(int[] sideLengths) {
+        Arrays.sort(sideLengths);
+        double maxSide = Math.pow(sideLengths[2], 2);
+        double remainingSide = Math.pow(sideLengths[0], 2) + Math.pow(sideLengths[1], 2);
+        return maxSide == remainingSide;
+    }
+
+    private static int[] getSideLengths(String lengths) {
+        StringTokenizer tokenizer = new StringTokenizer(lengths);
+
+        int[] sideLengths = new int[NUMBER_OF_SIDES];
+        for (int i = 0; i < NUMBER_OF_SIDES; i++) {
+            sideLengths[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        return sideLengths;
     }
 }
