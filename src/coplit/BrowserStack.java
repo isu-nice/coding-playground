0b45b31abd2efde6708ddbda2dc8d9b61f38a1ad
isu-nice <isu010614@naver.com>
2023-03-16 17:05:54 +0900

    feat: solve problem

---
+++ src/coplit/BrowserStack.java
@@ -0,0 +1,49 @@
+package coplit;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class BrowserStack {
+    public ArrayList<Stack> browserStack(String[] actions, String start) {
+        final String PREV = "-1";
+        final String NEXT = "1";
+
+        Stack<String> prevStack = new Stack<>();
+        Stack<String> nextStack = new Stack<>();
+        Stack<String> current = new Stack<>();
+        ArrayList<Stack> result = new ArrayList<>();
+
+        current.push(start);
+
+        for (String action : actions) {
+            String currentPage = current.peek();
+
+            if (action.equals(PREV)) {
+                if (!prevStack.isEmpty()) {
+                    nextStack.push(currentPage);
+                    current.pop();
+                    String prev = prevStack.pop();
+                    current.push(prev);
+                }
+            } else if (action.equals(NEXT)) {
+                if (!nextStack.isEmpty()) {
+                    prevStack.push(currentPage);
+                    current.pop();
+                    String next = nextStack.pop();
+                    current.push(next);
+                }
+            } else { // 새로운 페이지
+                current.pop();
+                prevStack.push(currentPage);
+                current.push(action);
+                nextStack.clear();
+            }
+        }
+
+        result.add(prevStack);
+        result.add(current);
+        result.add(nextStack);
+
+        return result;
+    }
+}
