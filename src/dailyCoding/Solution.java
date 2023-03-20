fe589a672620afc05fbc953427ade91bf0032d3b
isu-nice <isu010614@naver.com>
2023-03-20 10:06:46 +0900

    feat: solve problem

--- src/dailyCoding/Solution.java
+++ src/dailyCoding/Solution.java
@@ -2,4 +2,13 @@ package dailyCoding;
 // TODO 패키지 밑으로 복사
 
 public class Solution {
+    public String convertDoubleSpaceToSingle(String str) {
+        return str.replaceAll(" {2}", " ");
+    }
+
+    // 공부용
+    public String convertDoubleSpaceToSingle2(String str) {
+        String[] words = str.split(" {2}");
+        return String.join(" ", words);
+    }
 }
