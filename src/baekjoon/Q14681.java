c1d6e3c8feeedf0217c055362fd8cb72985bed4b
isu-nice <isu010614@naver.com>
2023-03-22 10:01:23 +0900

    [chore] 컨벤션 정리

--- src/baekjoon/Q14681.java
+++ src/baekjoon/Q14681.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class Q14681 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		int answer = 0;
-		
-		if(x > 0) {
-			if(y > 0) answer = 1;
-			else answer = 4;
-		}else{
-			if(y > 0) answer = 2;
-			else answer = 3;
-		}
+    public static void main(String[] args) {
 
-		System.out.println(answer);
-		
-		sc.close();
-	}
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int answer = 0;
+
+        if (x > 0) {
+            if (y > 0) answer = 1;
+            else answer = 4;
+        } else {
+            if (y > 0) answer = 2;
+            else answer = 3;
+        }
+
+        System.out.println(answer);
+
+        sc.close();
+    }
 }
