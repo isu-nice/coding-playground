616d5afed37e7aef5986204fb8c6c6eddc1fce92
isu-nice <isu010614@naver.com>
2023-03-26 15:00:24 +0900

    feat: solve problem

--- src/coplitPractice/ConnectedVertices.java
+++ src/coplitPractice/ConnectedVertices.java
@@ -4,7 +4,16 @@ import java.util.Arrays;
 
 public class ConnectedVertices {
     public int connectedVertices(int[][] edges) {
-        // edges 중 가장 큰 수를 찾는다
+        int group = 0;
+
+        // 최대 edge를 구한다.
+        int max = 0;
+        for (int[] edge : edges) {
+            int edgeMax = Arrays.stream(edge).reduce(max, Integer::max);
+            max = Math.max(edgeMax, max);
+        }
+
+        /* 최대값 구하는 방법 2
         final int[] max = {0};
         Arrays.stream(edges)
                 .forEach(line -> {
@@ -17,47 +26,42 @@ public class ConnectedVertices {
                         max[0] = biggest;
                     }
                 });
+        */
 
-        // 최대값을 배열의 길이로 지정
-        int[][] paths = new int[max[0] + 1][max[0] + 1];
-
-        // 간선을 연결해준다
+        // 간선 정보를 입력한다.
+        int[][] matrix = new int[max + 1][max + 1];
         for (int[] edge : edges) {
             int from = edge[0];
             int to = edge[1];
 
-            paths[from][to] = 1;
-            paths[to][from] = 1;
+            matrix[from][to] = 1;
+            matrix[to][from] = 1;
         }
 
-        // 방문 배열 선언
-        boolean[] visited = new boolean[max[0] + 1]; // 모두 false로 초기화됨
-        int group = 0;
+        // 방문 여부를 담을 배열 선언
+        boolean[] visited = new boolean[max + 1];
 
-        for (int vertex = 0; vertex <= max[0]; vertex++) {
-            // 끊기는 간선이 있을 때까지 탐색 -> 끊기면 그룹 + 1
-            if (!visited[vertex]) {
+        for (int vertex = 0; vertex < visited.length; vertex++) {
+            // 끊긴 구간이 있을 때 그룹 +1 해준다.
+            if(!visited[vertex]){
                 group++;
-                visited = dfs(paths, vertex, visited);
+                dfs(visited, matrix, vertex);
             }
         }
 
         return group;
     }
 
-    public boolean[] dfs(int[][] paths, int vertex, boolean[] visited) {
-        // 방문 표시
+    public void dfs(boolean[] visited, int[][] matrix, int vertex) {
+        // 방문 체크
         visited[vertex] = true;
 
-        for (int i = 0; i < paths.length; i++) {
-            // 경로가 존재하고 방문하지 않은 경우
-            if (paths[vertex][i] == 1 && !visited[i]) {
-                // 이어진 경로부터 다시 탐색
-                dfs(paths, i, visited);
+        for(int i = 0; i < matrix.length; i++){
+            // 방문하지 않았고, 간선이 연결되어 있는 경우
+            if(!visited[i] && matrix[vertex][i] == 1) {
+                // 그 자리부터 다시 탐색
+                dfs(visited, matrix, i);
             }
         }
-
-        // 이어진 길을 모두 순회한 후 방문여부 체크된 배열 리턴
-        return visited;
     }
 }
