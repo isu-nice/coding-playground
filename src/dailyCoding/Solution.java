32327e50a6b84d3464812883467d413c8d2bd390
isu-nice <isu010614@naver.com>
2023-03-29 09:38:49 +0900

    feat: solve problem

--- src/dailyCoding/Solution.java
+++ src/dailyCoding/Solution.java
@@ -4,5 +4,15 @@ package dailyCoding;
 import java.util.*;
 
 public class Solution {
+    public Integer modulo(int num1, int num2) {
+        if (num2 == 0) {
+            return null;
+        }
 
+        while (num1 >= num2) {
+            num1 -= num2;
+        }
+
+        return num1;
+    }
 }
