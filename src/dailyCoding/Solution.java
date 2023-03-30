7cc18bd19d52780cc15e127038f4d8ac4031b7cc
isu-nice <isu010614@naver.com>
2023-03-30 09:59:21 +0900

    feat: solve problem

--- src/dailyCoding/Solution.java
+++ src/dailyCoding/Solution.java
@@ -4,15 +4,18 @@ package dailyCoding;
 import java.util.*;
 
 public class Solution {
-    public Integer modulo(int num1, int num2) {
-        if (num2 == 0) {
-            return null;
+    public boolean isIsogram(String str) {
+        str = str.toLowerCase();
+        char[] arr = str.toCharArray();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int k = i + 1; k < str.length(); k++) {
+                if (str.charAt(i) == arr[k]) {
+                    return false;
+                }
+            }
         }
 
-        while (num1 >= num2) {
-            num1 -= num2;
-        }
-
-        return num1;
+        return true;
     }
 }
