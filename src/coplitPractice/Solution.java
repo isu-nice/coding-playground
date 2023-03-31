8f820df83972ac3d0c2a891ac0050cd1301d578b
isu-nice <isu010614@naver.com>
2023-03-31 09:34:49 +0900

    feat: solve problem

--- src/coplitPractice/Solution.java
+++ src/coplitPractice/Solution.java
@@ -4,95 +4,42 @@ package coplitPractice;
 import java.util.*;
 
 public class Solution {
-    public int paveBox(Integer[] boxes) {
-        int max = 1;
+    public String barcode(int len) {
+        return dfs("", len);
+    }
 
-        // 박스가 하나보다 적은 경우 그대로 1을 리턴
-        if(boxes.length <= 1) {
-            return max;
+    public String dfs(String answer, int len) {
+        String[] nums = {"1", "2", "3"};
+
+        // 탈출 조건
+        if (answer.length() == len) {
+            return answer;
         }
 
-        // 박스 배열을 큐에 모두 담아줌
-        Queue<Integer> line = new LinkedList<>(Arrays.asList(boxes));
-
-        int count = 1;
-        int first = line.poll();
-
-        // 대기줄이 모두 없어질 때까지 진행
-        while(line.size() >= 1 && line.size() <= boxes.length) {
-            // 맨 앞사람이 뒷 사람보다 큰 경우, 카운트 해주고 대기열에서 빼줌
-            if(first >= line.peek()){
-                count++;
-                line.poll();
-
-                // 카운트가 최댓값보다 크면 갱신해줌
-                if(count > max) {
-                    max = count;
+        for (int a = 0; a < nums.length; a++) {
+            String temp = answer + nums[a];
+            if (isValid(temp)) {
+                String search = dfs(temp, len);
+                if (search != null) {
+                    return search;
                 }
-            } else {
-                // 작은 경우 다시 처음부터 카운트해야하므로 초기화해줌
-                count = 1;
-                // 맨 앞사람은 다음 사람으로 초기화해줌
-                first = line.poll();
             }
         }
 
-        return max;
+        return null;
     }
 
+    public boolean isValid(String str) {
+        StringBuilder builder = new StringBuilder(str);
+        String reverseStr = builder.reverse().toString();
 
-
-
-
-
-
-
-
-
-
-
-
-    public int queuePrinter(int bufferSize, int capacities, int[] documents) {
-        Queue<Integer> printer = new LinkedList<>();
-
-        // 버퍼 0으로 채워주기
-        for (int i = 0; i < bufferSize; i++) {
-            printer.add(0);
-        }
-
-        int time = 0;
-        int index = 0;
-
-        while (!printer.isEmpty() && index < documents.length) {
-            if (printer.peek() <= 0) {
-                printer.poll();
-                if (capacities > sumAll(printer) + documents[index]) {
-                    printer.add(documents[index]);
-                    index++;
-                } else {
-                    printer.add(0);
-                }
-
-            } else {
-                printer = reduce(printer);
+        for (int i = 1; i <= str.length() / 2; i++) {
+            String sub = reverseStr.substring(0, i);
+            String nextSub = reverseStr.substring(i, i + i);
+            if (sub.equals(nextSub)) {
+                return false;
             }
-
-            time++;
         }
-
-        return time;
+        return true;
     }
-
-    public Queue<Integer> reduce(Queue<Integer> printer) {
-        Queue<Integer> temp = new LinkedList<>();
-        for (Integer num : printer) {
-            temp.add(num - 1);
-        }
-        return temp;
-    }
-
-    public int sumAll(Queue<Integer> printer) {
-        return printer.stream().reduce(0, Integer::sum);
-    }
-
 }
