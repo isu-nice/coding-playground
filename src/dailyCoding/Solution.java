767aafbfd079a53c8631d724adbb0f07d2048351
isu-nice <isu010614@naver.com>
2023-04-02 23:49:23 +0900

    feat: solve problem (decryptCaesarCipher)

--- src/dailyCoding/Solution.java
+++ src/dailyCoding/Solution.java
@@ -18,6 +18,26 @@ public class Solution {
         return Math.max(answer1, answer2);
     }
 
+    public String decryptCaesarCipher(String str, int secret) {
+        StringBuilder answer = new StringBuilder();
 
+        for (int i = 0; i < str.length(); i++) {
+            // 공백은 그대로 공백으로 둔다
+            if (str.charAt(i) == ' ') {
+                answer.append(" ");
+                continue;
+            }
 
+            int decryptNum = str.charAt(i) - secret;
+
+            // 복호화 한 값이 a 보다 작은 값이 나올 경우 그만큼 빼준 값을 z부터 세어서 append 함
+            if (decryptNum < 'a') {
+                decryptNum = ('z' + 1) - ('a' - decryptNum);
+            }
+
+            answer.append((char) decryptNum);
+        }
+
+        return answer.toString();
+    }
 }
