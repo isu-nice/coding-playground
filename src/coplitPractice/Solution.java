82610b730bb4ffed42aca958c3cda92ff6835d78
isu-nice <isu010614@naver.com>
2023-04-23 10:00:01 +0900

    feat: solve problem

--- src/coplitPractice/Solution.java
+++ src/coplitPractice/Solution.java
@@ -4,21 +4,29 @@ package coplitPractice;
 import java.util.*;
 
 public class Solution {
-    public int partTimeJob(int k) {
-        int[] coins = new int[]{500, 100, 50, 10, 5, 1};
-        int num = 0;
-
-        for (int coin : coins) {
-            if(k == 0) {
-                break;
-            }
-
-            int count = k / coin;
-            num += count;
-            k -= (coin * count);
-        }
-
-        return num;
-    }
+//    public long ocean(int target, int[] type) {
+//        // type을 내림차순으로 정렬한다.
+//        Integer[] types = Arrays.stream(type)
+//                .boxed()
+//                .toArray(Integer[]::new);
+//
+//        Arrays.sort(types, Collections.reverseOrder());
+//
+//        boolean[] visited = new boolean[types.length];
+//        long answer = 0;
+//
+//        for (int i = 0; i < types.length; i++) {
+//            answer += search(type, visited, target, 0, 0);
+//            visited[i] = true;
+//        }
+//
+//        return answer;
+//    }
+//
+//    public long search(int[] type, boolean[] visited, int target, int count, int sum) {
+//        if(target > sum) {
+//            return count;
+//        }
+//    }
 }
 
