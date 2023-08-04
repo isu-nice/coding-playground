621fc4bfed1797da9b1c7c11fb87c133eef88a02
isu-nice <isu010614@naver.com>
2023-08-04 21:30:09 +0900

    [chore] 컨벤션 정리

--- src/baekjoon/Q1110.java
+++ src/baekjoon/Q1110.java
@@ -6,38 +6,38 @@ import java.io.InputStreamReader;
 
 public class Q1110 {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int primeNum = Integer.parseInt(br.readLine());
-		String resultNum = "-1";
-		int fir, sec, cnt = 0;
+        int primeNum = Integer.parseInt(br.readLine());
+        String resultNum = "-1";
+        int fir, sec, cnt = 0;
 
-		while(Integer.parseInt(resultNum) != primeNum) {
-			cnt ++;
+        while (Integer.parseInt(resultNum) != primeNum) {
+            cnt++;
 
-			if(resultNum.equals("-1")) resultNum = Integer.toString(primeNum);
+            if (resultNum.equals("-1")) resultNum = Integer.toString(primeNum);
 
-			if(Integer.parseInt(resultNum) < 10) {
-				fir = 0;
-				sec = Integer.parseInt(resultNum);
-			}else {
-				fir = Integer.parseInt(resultNum.substring(0, 1));
-				sec = Integer.parseInt(resultNum.substring(1));
-			}
+            if (Integer.parseInt(resultNum) < 10) {
+                fir = 0;
+                sec = Integer.parseInt(resultNum);
+            } else {
+                fir = Integer.parseInt(resultNum.substring(0, 1));
+                sec = Integer.parseInt(resultNum.substring(1));
+            }
 
-			resultNum = Integer.toString(fir + sec);
+            resultNum = Integer.toString(fir + sec);
 
-			if(fir + sec < 10) {
-				resultNum = Integer.toString(sec) + resultNum;
-			}else {
-				resultNum = Integer.toString(sec) + resultNum.substring(1);
-			}
+            if (fir + sec < 10) {
+                resultNum = Integer.toString(sec) + resultNum;
+            } else {
+                resultNum = Integer.toString(sec) + resultNum.substring(1);
+            }
 
-		}
+        }
 
-		System.out.println(cnt);
-	}
+        System.out.println(cnt);
+    }
 
 }
