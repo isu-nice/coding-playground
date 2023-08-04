6ae5a569589cd6947b04c1b214882d2eb3315336
isu-nice <isu010614@naver.com>
2023-08-04 21:30:27 +0900

    [chore] 컨벤션 정리

--- src/baekjoon/Q2588.java
+++ src/baekjoon/Q2588.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class Q2588 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int upNum = sc.nextInt();
-		int downNum = sc.nextInt();
-		int fir, sec, thr;
-		
-		fir = downNum%10; 
-		sec = downNum%100 - fir;
-		thr = downNum - fir - sec;
-		
-		System.out.println(upNum * fir);
-		System.out.println(upNum * sec / 10);
-		System.out.println(upNum * thr / 100);
-		System.out.println(upNum * downNum);
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int upNum = sc.nextInt();
+        int downNum = sc.nextInt();
+        int fir, sec, thr;
+
+        fir = downNum % 10;
+        sec = downNum % 100 - fir;
+        thr = downNum - fir - sec;
+
+        System.out.println(upNum * fir);
+        System.out.println(upNum * sec / 10);
+        System.out.println(upNum * thr / 100);
+        System.out.println(upNum * downNum);
+
+        sc.close();
+    }
 
 }
