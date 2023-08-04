4c8e49d01642c408ec8d0e4c0b91cb5be8ff8d8f
isu-nice <isu010614@naver.com>
2023-08-04 21:30:13 +0900

    [chore] 컨벤션 정리

--- src/baekjoon/Q1316.java
+++ src/baekjoon/Q1316.java
@@ -4,38 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
- 
+
 public class Q1316 {
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		HashSet<Character> set = new HashSet<>();
-		int count = n;
-		
-		for(int i = 1; i <= n; i++) {
-			String word = br.readLine();
-			set.add(word.charAt(0));
-			
-			for(int a = 1; a < word.length(); a++) {
-				
-				if(word.charAt(a-1) == word.charAt(a)) {
-					set.add(word.charAt(a));
-				}else {
-					
-					if(!set.contains(word.charAt(a))) {
-						set.add(word.charAt(a));
-					}else {
-						count--;
-						break;
-					}
-				}
-			}
-			set.clear();
-		}
-			
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        HashSet<Character> set = new HashSet<>();
+        int count = n;
+
+        for (int i = 1; i <= n; i++) {
+            String word = br.readLine();
+            set.add(word.charAt(0));
+
+            for (int a = 1; a < word.length(); a++) {
+
+                if (word.charAt(a - 1) == word.charAt(a)) {
+                    set.add(word.charAt(a));
+                } else {
+
+                    if (!set.contains(word.charAt(a))) {
+                        set.add(word.charAt(a));
+                    } else {
+                        count--;
+                        break;
+                    }
+                }
+            }
+            set.clear();
+        }
+
+        System.out.println(count);
+    }
 }
