b77031a9b6693d1c9293b676ab774cd9b55121ff
isu-nice <isu010614@naver.com>
2023-08-04 21:30:15 +0900

    [chore] 컨벤션 정리

--- src/baekjoon/Q1427.java
+++ src/baekjoon/Q1427.java
@@ -6,27 +6,27 @@ import java.io.InputStreamReader;
 
 public class Q1427 {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		String number = br.readLine();
-		
-		// 카운팅 정렬
-		int[] countingArr = new int[10];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        String number = br.readLine();
 
-		for(int i = 0; i < number.length(); i++) {
-			int n = number.charAt(i) - '0';
-			countingArr[n]++;
-		}
-		
-		for(int i = 9; i >= 0; i--) {
-			
-			if(countingArr[i] >= 1) {
-				sb.append((i + "").repeat(countingArr[i])); 
-			}
-		}
-		
-		System.out.println(sb);
-	}
+        // 카운팅 정렬
+        int[] countingArr = new int[10];
+
+        for (int i = 0; i < number.length(); i++) {
+            int n = number.charAt(i) - '0';
+            countingArr[n]++;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+
+            if (countingArr[i] >= 1) {
+                sb.append((i + "").repeat(countingArr[i]));
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
