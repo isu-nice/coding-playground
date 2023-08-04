6906dd9ccf68cea8617960692f81c4e727851a9b
isu-nice <isu010614@naver.com>
2023-08-04 21:30:30 +0900

    [chore] 컨벤션 정리

--- src/baekjoon/Q2798.java
+++ src/baekjoon/Q2798.java
@@ -1,30 +1,30 @@
 package baekjoon;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Q2798 {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int card[] = new int[100];
-		int ans = 0;
-		
-		for (int i = 0; i < N; i++) {
-			card[i] = sc.nextInt();
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = i+1; j < N; j++) {
-				for (int k = j+1; k < N; k++) {
-					int sum = card[i]+card[j]+card[k];
-					if(sum <= M) ans = Math.max(ans, sum);
-				}
-			}
-		}
-		sc.close();
-		System.out.println(ans);
-	}
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int card[] = new int[100];
+        int ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            card[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = card[i] + card[j] + card[k];
+                    if (sum <= M) ans = Math.max(ans, sum);
+                }
+            }
+        }
+        sc.close();
+        System.out.println(ans);
+    }
 }
