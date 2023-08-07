e1e192d2ca7009c5d71a4d87739ecbb11fcf6f18
isu-nice <isu010614@naver.com>
2023-08-07 15:43:20 +0900

    [feat] 문제 풀이

--- src/baekjoon/Q2525.java
+++ src/baekjoon/Q2525.java
@@ -7,21 +7,21 @@ import java.util.StringTokenizer;
 
 public class Q2525 {
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int hour = Integer.parseInt(st.nextToken());
-		int minute = Integer.parseInt(st.nextToken());
-		int time = Integer.parseInt(br.readLine());
-		
-		int rest = (minute + time) % 60;
-		hour += (minute + time - rest) / 60;
-		minute = rest;
-		
-		if (hour >= 24) hour = hour - 24;
-		
-		System.out.println(hour + " " + minute);
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+        int time = Integer.parseInt(br.readLine());
+
+        int rest = (minute + time) % 60;
+        hour += (minute + time - rest) / 60;
+        minute = rest;
+
+        if (hour >= 24) hour = hour - 24;
+
+        System.out.println(hour + " " + minute);
+    }
 }
