25015d48464bdfc73a2a80508080aa81dab9ba14
isu-nice <isu010614@naver.com>
2023-08-07 15:43:25 +0900

    [feat] 문제 풀이

--- src/baekjoon/Q10430.java
+++ src/baekjoon/Q10430.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Q10430 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		int D = A%C;
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = A % C;
 
-		System.out.println((A+B)%C);
-		System.out.println(((D) + (B%C))%C);
-		System.out.println((A*B)%C);
-		System.out.println((D*(B%C)) % C);
+        System.out.println((A + B) % C);
+        System.out.println(((D) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println((D * (B % C)) % C);
 
-		sc.close();
-	}
+        sc.close();
+    }
 
 }
