dcce3173362841d6c0a0f402cc0e30332b8d5426
isu-nice <isu010614@naver.com>
2023-08-07 23:44:07 +0900

    [feat] 문제 풀이

--- src/baekjoon/Q2869.java
+++ src/baekjoon/Q2869.java
@@ -7,18 +7,18 @@ import java.util.StringTokenizer;
 
 public class Q2869 {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int up = Integer.parseInt(st.nextToken());
-		int down = Integer.parseInt(st.nextToken());
-		int height = Integer.parseInt(st.nextToken());
-		int go = up - down;
-		double day = (double) (height - up) / go;
-		
-		System.out.println((int)Math.ceil(day) + 1);
-		
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int up = Integer.parseInt(st.nextToken());
+        int down = Integer.parseInt(st.nextToken());
+        int height = Integer.parseInt(st.nextToken());
+        int go = up - down;
+        double day = (double) (height - up) / go;
+
+        System.out.println((int) Math.ceil(day) + 1);
+
+    }
 }
