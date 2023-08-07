7e99daaf689fa23a6a8e7fa6cc9da37b84e18b95
isu-nice <isu010614@naver.com>
2023-08-07 23:44:10 +0900

    [feat] 문제 풀이

--- src/baekjoon/Q4344.java
+++ src/baekjoon/Q4344.java
@@ -7,36 +7,36 @@ import java.util.StringTokenizer;
 
 public class Q4344 {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int caseNum = Integer.parseInt(br.readLine());
-		int studentNum, sum, cnt; 
-		int[] score; double answer;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNum = Integer.parseInt(br.readLine());
+        int studentNum, sum, cnt;
+        int[] score;
+        double answer;
 
-		for(int i = 1; i <= caseNum; i++) {
+        for (int i = 1; i <= caseNum; i++) {
 
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			studentNum = Integer.parseInt(st.nextToken());
-			score = new int[studentNum];
-			sum = 0; cnt = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            studentNum = Integer.parseInt(st.nextToken());
+            score = new int[studentNum];
+            sum = 0;
+            cnt = 0;
 
-			for(int j = 0; j < studentNum; j++) {
-				score[j] = Integer.parseInt(st.nextToken());
-				sum += score[j];
-			}
+            for (int j = 0; j < studentNum; j++) {
+                score[j] = Integer.parseInt(st.nextToken());
+                sum += score[j];
+            }
 
-			double avg = (double) sum/studentNum;
+            double avg = (double) sum / studentNum;
 
-			for(int j = 0; j < studentNum; j++) {
-				if(score[j] > avg) cnt++;
-			}
+            for (int j = 0; j < studentNum; j++) {
+                if (score[j] > avg) cnt++;
+            }
 
-			answer = (double) cnt*100/studentNum;
-			System.out.println(String.format("%.3f", answer) + "%");
-
-		}
-
-	}
+            answer = (double) cnt * 100 / studentNum;
+            System.out.println(String.format("%.3f", answer) + "%");
+        }
+    }
 
 }
