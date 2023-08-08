ca52b3e51934c57f41daa5047b36aabe5f793ac6
isu-nice <isu010614@naver.com>
2023-08-08 13:15:41 +0900

    [feat] 문제 풀이

--- src/baekjoon/Q11654.java
+++ src/baekjoon/Q11654.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Q11654 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String answer = sc.nextLine();
-		sc.close();
+    public static void main(String[] args) {
 
-		char ascii = answer.charAt(0);
-		
-		System.out.println((int)ascii);
-	}
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+        sc.close();
+
+        char ascii = answer.charAt(0);
+
+        System.out.println((int) ascii);
+    }
 }
