5d5e38c5e5340475059b13ba499744813f7dfe13
isu-nice <isu010614@naver.com>
2023-08-09 22:14:25 +0900

    [feat] 문제 풀이

--- src/programmersLevel1/MarathonFailure.java
+++ src/programmersLevel1/MarathonFailure.java
@@ -3,21 +3,22 @@ package programmersLevel1;
 import java.util.Arrays;
 
 public class MarathonFailure {
-	public String solution(String[] participant, String[] completion) {
-		String answer = "";
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
 
-		Arrays.sort(participant);
-		Arrays.sort(completion);
+        Arrays.sort(participant);
+        Arrays.sort(completion);
 
-		int n = completion.length;
-		
-		for (int i = 0; i < n; i++) {
-			
-			if (! participant[i].equals(completion[i])) {
-				return answer = participant[i];
-			}
-		}
-		answer = participant[n];
-		return answer;
-	}
+        int n = completion.length;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+
+        answer = participant[n];
+        return answer;
+    }
 }
