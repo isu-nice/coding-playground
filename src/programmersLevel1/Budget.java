05e25643f14ada12356c585ae45c1aaa2222d06b
isu-nice <isu010614@naver.com>
2023-08-13 10:00:23 +0900

    [refactor] 코드 리팩토링

--- src/programmersLevel1/Budget.java
+++ src/programmersLevel1/Budget.java
@@ -3,17 +3,17 @@ package programmersLevel1;
 import java.util.Arrays;
 
 class Budget {
-	public int solution(int[] d, int budget) {
-		int answer = 0;
-		Arrays.sort(d);
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
 
-		for(int i = 0; i < d.length; i++) {
-			budget -= d[i];
-			
-			if (budget >= 0) answer += 1;
-			else return answer;
-		}
+        for (int j : d) {
+            budget -= j;
 
-		return answer;
-	}
+            if (budget >= 0) answer += 1;
+            else return answer;
+        }
+
+        return answer;
+    }
 }
