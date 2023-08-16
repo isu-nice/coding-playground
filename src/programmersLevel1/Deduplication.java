ae31b9eb1bf5eaece48ea7ae47a6620d76df74df
isu-nice <isu010614@naver.com>
2023-08-16 10:00:23 +0900

    [feat] 문제 풀이

--- src/programmersLevel1/Deduplication.java
+++ src/programmersLevel1/Deduplication.java
@@ -3,26 +3,27 @@ package programmersLevel1;
 import java.util.ArrayList;
 
 public class Deduplication {
-	public int[] solution(int []arr) {
-		int[] answer = {};
-		int len = arr.length;
-		ArrayList<Integer> arrList =  new ArrayList<Integer>();
-		arrList.add(arr[0]);
-		
-		int cnt = 0;
-		for (int i = 1; i < len; i++) {
-			if (! arrList.get(cnt).equals(arr[i])) {
-				arrList.add(arr[i]);
-				cnt++;
-			}
-		}
-		
-		answer = arrList.stream()
-				.mapToInt(Integer::intValue)
-				.toArray();
-		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-		//System.out.println(Arrays.toString(answer));
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        int len = arr.length;
 
-		return answer;
-	}
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        arrList.add(arr[0]);
+
+        int cnt = 0;
+        for (int i = 1; i < len; i++) {
+            if (!arrList.get(cnt).equals(arr[i])) {
+                arrList.add(arr[i]);
+                cnt++;
+            }
+        }
+
+        answer = arrList.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        //System.out.println(Arrays.toString(answer));
+
+        return answer;
+    }
 }
