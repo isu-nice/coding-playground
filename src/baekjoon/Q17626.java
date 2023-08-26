58de99467202d26dd939524fafc5bcd7a403e3ca
isu-nice <isu010614@naver.com>
2023-08-26 10:10:23 +0900

    [feat] BOJ 17626 문제 풀이

---
+++ src/baekjoon/Q17626.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 점화식 다시 풀어보기
+public class Q17626 {
+
+    public static void main(String[] args) throws IOException {
+        new Q17626().run();
+    }
+
+    private void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+
+        int[] dp = new int[number + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+
+        // 점화식
+        // dp[i] = min(dp[i - a*a]) + 1
+        for (int index = 2; index <= number; index++) {
+            int min = Integer.MAX_VALUE;
+
+            for (int a = 1; a * a <= index; a++) {
+                min = Math.min(min, dp[index - a * a]);
+            }
+
+            dp[index] = min + 1;
+        }
+
+        System.out.println(dp[number]);
+    }
+}
