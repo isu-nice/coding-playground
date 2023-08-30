6743f5b320019c69b3c957ce5f6878ddf3087457
isu-nice <isu010614@naver.com>
2023-08-30 07:24:51 +0200

    [feat] BOJ 2798 문제 풀이 추가

--- src/baekjoon/Q2798.java
+++ src/baekjoon/Q2798.java
@@ -2,6 +2,58 @@ package baekjoon;
 
 import java.util.Scanner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2798 {
+
+    public static void main(String[] args) throws IOException {
+        // newQ2798.run2(); // 초기 불이 방법
+        new Q2798().run();  // 2023년 8월 재풀이
+    }
+
+    private void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        int cardNum = Integer.parseInt(tokenizer.nextToken());
+        int dealer = Integer.parseInt(tokenizer.nextToken());
+
+        tokenizer = new StringTokenizer(reader.readLine());
+        int[] cards = new int[cardNum];
+
+        for (int index = 0; index < cardNum; index++) {
+            cards[index] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        int result = startGame(cards, dealer);
+
+        System.out.println(result);
+    }
+
+    private int startGame(int[] cards, int dealer) {
+        int max = 0;
+        int num = cards.length;
+
+        for (int i = 0; i < num - 2; i++) {
+            for (int j = i + 1; j < num - 1; j++) {
+                for (int k = j + 1; k < num; k++) {
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if (sum <= dealer) {
+                        max = Math.max(max, sum);
+                    }
+                }
+            }
+        }
+
+        return max;
+    }
+}
+
+
+
 public class Q2798 {
 
     public static void main(String[] args) {
