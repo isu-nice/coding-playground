318c2aa2c2425ef8a7759343dd9bccaa43fa40f2
isu-nice <isu010614@naver.com>
2023-08-30 07:00:31 +0200

    [feat] 문제 풀이

--- src/programmersLevel1/AlphabetInString.java
+++ src/programmersLevel1/AlphabetInString.java
@@ -7,23 +7,25 @@ class AlphabetInString {
     boolean solution(String s) {
         boolean answer = true;
         int len = s.length();
-        
+
         Queue<Character> q = new LinkedList<>();
-        
-        for(int i = 0; i < len; i++) {
-        	q.add(s.charAt(i));
+
+        for (int i = 0; i < len; i++) {
+            q.add(s.charAt(i));
         }
-        
-        int cntP = 0;  int cntY = 0;
-        for(int i = 0; i < len; i++) {
-        	int alphabet = q.poll();
-        	if(alphabet == 'p' || alphabet == 'P') cntP++;
-        	if(alphabet == 'y' || alphabet == 'Y') cntY++;
+
+        int cntP = 0;
+        int cntY = 0;
+
+        for (int i = 0; i < len; i++) {
+            int alphabet = q.poll();
+            if (alphabet == 'p' || alphabet == 'P') cntP++;
+            if (alphabet == 'y' || alphabet == 'Y') cntY++;
         }
-        
-        if(cntP != cntY) return false; 
-      
+
+        if (cntP != cntY) return false;
+
         return answer;
     }
-    
+
 }
